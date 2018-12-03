dddebb8b7e9f1c75f8edae2f87498f6a34b0a76b
troh21 <45541788+troh21@users.noreply.github.com>
2018-12-02 23:01:33 -0500

    Update GeoDash.java

--- GeoDash.java
+++ GeoDash.java
@@ -4,8 +4,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.util.Random;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-public class GeoDash extends JPanel{
+public class GeoDash extends JPanel implements KeyListener{
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
     public static final int FPS = 60;
@@ -15,35 +17,26 @@ public class GeoDash extends JPanel{
     //Note: The following are not used yet, you should use them in writing your code.
     double velocityX;
     double velocityY;
-    double accelerationX;
     double accelerationY;
     
     Sphere Something = new Sphere(positionX, positionY);    
     class Runner implements Runnable{
         public Runner()
         {
-            //Feel free to change these default values
             positionX = 275;  
             positionY = HEIGHT - 275;
             velocityX = 100; 
             velocityY = -100; 
-            accelerationY = 98;
-            //your code here for adding the second sphere
-			
-			 
+            accelerationY = 98;	 
         }
+        
         public void run()
         {
             while(true){
-                //your code here
-                //Implement Movement  here
-                //(Use velocityX and velocityY rather than fixed constants)
             		Something.move(); 	
-                //Implement bouncing here
             		Something.bounce();
             	
                 //Implement gravity here (Bonus)
-                //don't mess too much with the rest of this method
                 repaint();
                 try{
                     Thread.sleep(1000/FPS);
@@ -51,9 +44,35 @@ public class GeoDash extends JPanel{
                 catch(InterruptedException e){}
             }
         }    
+        
+        
+    }
+    
+    public void keyPressed(KeyEvent e) {
+    	char c=e.getKeyChar();	
+    	System.out.println("You pressed down: " + c);
+    	if (c == ' ') 
+    		Something.Up(true);
+    }
+    public void keyReleased(KeyEvent e) {
+    	char c=e.getKeyChar();
+    	System.out.println("\tYou let go of: " + c);
+    	if (c == ' ') 
+    		Something.Up(false);
+
+    }
+    public void keyTyped(KeyEvent e) {
+    	char c = e.getKeyChar();
+    	System.out.println("You typed: " + c);
+    	
+    }
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
     }
 
     public GeoDash(){
+        addKeyListener(this);
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         Thread mainThread = new Thread(new Runner());
         mainThread.start();
@@ -94,10 +113,12 @@ class Sphere {
     double positionX = 275;
     double positionY = 768 - 275;
     double velocityX = 0;
-    double velocityY = 1000;
+    double velocityY = 0;
+    double gravity = 0.5;
+    boolean up;
+    
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
-    public static final int FPS = 60;
     public static final int RADIUS = 50;
 
 	public Sphere(double positionX, double positionY) {
@@ -105,21 +126,30 @@ class Sphere {
 		this.positionY = positionY;
 	}
 	public void move() {
-		 this.positionX += velocityX / (double)FPS; 
-         this.positionY += velocityY / (double)FPS; 
-	}
+
+         this.positionY += velocityY; 
+         this.velocityY += gravity;
+         
+         if (up) 
+        	 velocityY += -2;
+    }
+
 	public void bounce() {
-		 if (this.positionX < 0) {
-           	velocityX = -velocityX;
-           }
-         else if (this.positionX + RADIUS > WIDTH) { //hello
-           	velocityX = -velocityX;
-           }   
+		
+       
          if (this.positionY < 0) {
            	velocityY = -velocityY;
            }
          else if (this.positionY + RADIUS > HEIGHT) {
-           	velocityY = -velocityY;
+        	positionY = HEIGHT - RADIUS;
+           	velocityY = 0;
            }
 	}
+	public void Up(boolean input) {
+		up = input;
+	}
+	public void notUp (boolean input) {
+		up = input;
+	}
+
 }
